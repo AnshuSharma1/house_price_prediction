9da5eecb86e57c7e8f7d87a6ea98f3e1a788e6b5
Anshu Sharma <anshu.sharma@truebil.com>
2020-04-13 00:10:00 +0530

    Prediction API added

---
+++ app.py
@@ -0,0 +1,73 @@
+import pickle
+
+import redis
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import model_from_json
+
+from contants import REDIS_CONNECTION_URL
+
+app = Flask(__name__)
+
+
+def get_redis_connection():
+    return redis.Redis().from_url(REDIS_CONNECTION_URL)
+
+
+redis_con = get_redis_connection()
+
+
+@app.route('/')
+def root():
+    url_list = [
+        request.url + 'predict_price/?bedrooms=3&bathrooms=1&sqft_living=1180&sqft_lot=5650&'
+                      'floors=1&condition=2&grade=7&sqft_above=1180&sqft_basement=0&orig_age=59'
+    ]
+    return jsonify(url_list)
+
+
+@app.route('/predict_price/')
+def predict():
+    """
+    [3, 1, 1180, 5650, 1.0, 3, 7, 1180, 0, 59]
+    :return:
+    """
+    bedrooms = int(request.args.get('bedrooms', None))
+    sqft_living = int(request.args.get('sqft_living', None))
+    floors = int(request.args.get('floors', None))
+    orig_age = int(request.args.get('orig_age', None))
+    bathrooms = int(request.args.get('bathrooms', 1))
+    sqft_lot = int(request.args.get('sqft_lot', 500))
+    condition = int(request.args.get('condition', 2))
+    grade = int(request.args.get('grade', 3))
+    sqft_above = int(request.args.get('sqft_above', 500))
+    sqft_basement = int(request.args.get('sqft_basement', 0))
+
+    if None in [bedrooms, sqft_living, floors, orig_age]:
+        return 'Insufficient args'
+
+    try:
+        reg_model = pickle.loads(redis_con.get('reg_model'))
+        seq_model = pickle.loads(redis_con.get('seq_model'))
+        seq_model = model_from_json(seq_model)
+        min_max_scaler = pickle.loads(redis_con.get('min_max_scaler'))
+    except TypeError:
+        return 'Key does not exist'
+
+    scaled_data = min_max_scaler.transform([[bedrooms, bathrooms, sqft_living,
+                                             sqft_lot, floors, condition, grade,
+                                             sqft_above, sqft_basement, orig_age]])
+
+    reg_price = float(reg_model.predict(scaled_data)[0])
+    seq_price = float(seq_model.predict(scaled_data)[0][0])
+
+    response = {
+        'reg_predicted_price': reg_price,
+        'seq_predicted_price': seq_price
+    }
+
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    # Hit http://127.0.0.1:5000/?
+    app.run(debug=True)
